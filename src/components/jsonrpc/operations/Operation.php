fe4182e675dd4cb9301d4cc4eec0256f001cea13
jeyroik <jeyroik@gmail.com>
2020-01-14 13:05:59 +0300

    added missed methods

--- src/components/jsonrpc/operations/Operation.php
+++ src/components/jsonrpc/operations/Operation.php
@@ -6,6 +6,7 @@ use extas\components\THasClass;
 use extas\components\THasDescription;
 use extas\components\THasName;
 use extas\interfaces\jsonrpc\operations\IOperation;
+use extas\interfaces\jsonrpc\operations\IOperationFilter;
 
 /**
  * Class Operation
@@ -18,6 +19,108 @@ class Operation extends Item implements IOperation
     use THasName;
     use THasDescription;
     use THasClass;
+
+    /**
+     * @return string
+     */
+    public function getItemName(): string
+    {
+        return $this->config[static::FIELD__ITEM_NAME] ?? '';
+    }
+
+    /**
+     * @return string
+     */
+    public function getItemClass(): string
+    {
+        return $this->config[static::FIELD__ITEM_CLASS] ?? '';
+    }
+
+    /**
+     * @return string
+     */
+    public function getItemRepo(): string
+    {
+        return $this->config[static::FIELD__ITEM_REPO] ?? '';
+    }
+
+    /**
+     * @return IOperationFilter|null
+     */
+    public function getFilter(): ?IOperationFilter
+    {
+        $filterClass = $this->config[static::FIELD__FILTER] ?? '';
+
+        return $filterClass ? new $filterClass() : null;
+    }
+
+    /**
+     * @return string
+     */
+    public function getMethod(): string
+    {
+        return $this->config[static::FIELD__METHOD] ?? '';
+    }
+
+    /**
+     * @param string $itemName
+     *
+     * @return IOperation
+     */
+    public function setItemName(string $itemName): IOperation
+    {
+        $this->config[static::FIELD__ITEM_NAME] = $itemName;
+
+        return $this;
+    }
+
+    /**
+     * @param string $itemClass
+     *
+     * @return IOperation
+     */
+    public function setItemClass(string $itemClass): IOperation
+    {
+        $this->config[static::FIELD__ITEM_CLASS] = $itemClass;
+
+        return $this;
+    }
+
+    /**
+     * @param string $repoName
+     *
+     * @return IOperation
+     */
+    public function setItemRepo(string $repoName): IOperation
+    {
+        $this->config[static::FIELD__ITEM_REPO] = $repoName;
+
+        return $this;
+    }
+
+    /**
+     * @param IOperationFilter $filter
+     *
+     * @return IOperation
+     */
+    public function setFilter(IOperationFilter $filter): IOperation
+    {
+        $this->config[static::FIELD__FILTER] = get_class($filter);
+
+        return $this;
+    }
+
+    /**
+     * @param string $method
+     *
+     * @return IOperation
+     */
+    public function setMethod(string $method): IOperation
+    {
+        $this->config[static::FIELD__METHOD] = $method;
+
+        return $this;
+    }
 
     /**
      * @return array
